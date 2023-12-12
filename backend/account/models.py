cf0b5d66859ee23e091667040ddd6e3da4d96ff8
Amin Halvaei <aminhalvaei80@gmail.com>
2023-12-12 19:28:49 +0330

    custom user class created

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        _("email address"),
+        help_text=_("Required. please enter a valid email address."),
+        blank=False,
+    )
+    birthdate = models.DateField(_("Birthday"), blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ["email", "password", "birthdate"]
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
