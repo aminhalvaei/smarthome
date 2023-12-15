7110f7c2941123d00e78c7a04a58a577469eb15e
Amin Halvaei <aminhalvaei80@gmail.com>
2023-12-15 18:49:51 +0330

    parameter and its category views created

--- backend/weather/views.py
+++ backend/weather/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Parameter, ParameterCategory
+from .serializers import ParameterSerializer, ParameterCategorySerializer
 
 # Create your views here.
+
+
+class ParameterList(generics.ListCreateAPIView):
+    queryset = Parameter.objects.all()
+    serializer_class = ParameterSerializer
+
+
+class ParameterDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Parameter.objects.all()
+    serializer_class = ParameterSerializer
+
+
+class ParameterCategoryList(generics.ListAPIView):
+    queryset = ParameterCategory.objects.all()
+    serializer_class = ParameterCategorySerializer
+
+
+class ParameterCategoryDetail(generics.RetrieveAPIView):
+    queryset = ParameterCategory.objects.all()
+    serializer_class = ParameterCategorySerializer
