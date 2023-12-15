3b04e70c493399e290ae3b8f326e2f29dc087038
Amin Halvaei <aminhalvaei80@gmail.com>
2023-12-15 17:49:56 +0330

    parameter and its category registered on admin

--- backend/weather/admin.py
+++ backend/weather/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Parameter, ParameterCategory
+
 # Register your models here.
+
+admin.site.register(Parameter)
+admin.site.register(ParameterCategory)
+
