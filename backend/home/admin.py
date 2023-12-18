faab02066de07ea289110320ea504320322ea095
Amin Halvaei <aminhalvaei80@gmail.com>
2023-12-18 18:06:05 +0330

    city,loc,home registered and migrated

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import City, Location, Home
+
 # Register your models here.
+
+admin.site.register(City)
+admin.site.register(Location)
+admin.site.register(Home)
+
+
