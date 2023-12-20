b0e6a6e76918e65f8c5eb504949fe414edb33836
Amin Halvaei <aminhalvaei80@gmail.com>
2023-12-20 12:30:45 +0330

    home app models serializers created

---
+++ backend/home/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+
+from .models import City, Location, Home, Controller, Device
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'name',
+            'country',
+            'province',
+        )
+        model = City
+        
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'city',
+            'latitude',
+            'longitude',
+        )
+        model = Location
+        
+class HomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'alias_name',
+            'user',
+            'location',
+        )
+        model = Home
+        
+class ControllerSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'physical_id',
+            'home',
+            'alias_name',
+            
+        )
+        model = Controller
+        
+class DeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'physical_id',
+            'controller',
+            'alias_name',
+        )
+        model = Device
