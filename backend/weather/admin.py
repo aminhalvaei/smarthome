f756c287728e0cc7c5aea02ce113f7bd3dfa36e3
Amin Halvaei <aminhalvaei80@gmail.com>
2023-12-24 19:00:29 +0330

    new weather apps registered and migrated

--- backend/weather/admin.py
+++ backend/weather/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from .models import Parameter, ParameterCategory
+from .models import (
+    Parameter,
+    ParameterCategory,
+    Preference,
+    Config,
+    WeatherCondition,
+    ParameterValue,
+)
 
 # Register your models here.
 
 admin.site.register(Parameter)
 admin.site.register(ParameterCategory)
-
+admin.site.register(Preference)
+admin.site.register(Config)
+admin.site.register(WeatherCondition)
+admin.site.register(ParameterValue)
