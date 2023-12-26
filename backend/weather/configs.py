ad34aef9e8256e2434116e1a671c7d457ba147b5
Amin Halvaei <aminhalvaei80@gmail.com>
2023-12-26 22:28:38 +0330

    Update weather configs

--- backend/weather/configs.py
+++ backend/weather/configs.py
@@ -3,3 +3,13 @@ from datetime import timedelta
 # Its the duration that a weather condition is valid
 # so it does not need to make an new api call to get weather
 WEATHER_VALID_DURATION = timedelta(milliseconds=5000)
+
+UNITS_OF_MEASUREMENT = 'metric'
+
+CONSTANT_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+# Here is a list of available api keys from
+# openweathermap.org
+WEATHER_API_KEYS = [
+    "46eb760d100fb6baef9ac0517c1123d8",
+]
