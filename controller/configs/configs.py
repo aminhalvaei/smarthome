d147623f54e97e9cce70ed2df521bf79b8db33ff
Amin Halvaei <aminhalvaei80@gmail.com>
2024-01-03 17:54:10 +0330

    Local configs file created as a module

---
+++ controller/configs/configs.py
@@ -0,0 +1,8 @@
+# Local Controller settings are stored in this file
+# Including server_url controller_physical_id and ...
+
+SERVER_URL = "http://127.0.0.1:8000"
+
+CONTROLLER_PHYSICAL_ID = "12ab89ef6548"
+
+VALIDATION_INTERVAL_IN_SECONDS = 15
