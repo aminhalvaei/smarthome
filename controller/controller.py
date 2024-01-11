115d76832c5a87774ddef730a326d8a4e0124152
Amin Halvaei <aminhalvaei80@gmail.com>
2024-01-11 16:51:43 +0330

    Performs registration at startup

--- controller/controller.py
+++ controller/controller.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, BackgroundTasks
 from datetime import datetime, timedelta
 import asyncio
 import httpx
+import json
 
 from configs import configs
 
@@ -11,32 +12,50 @@ async def get_weather_condition():
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{configs.SERVER_URL}/api/cservice/weather-condition/", params={"physical_id": configs.CONTROLLER_PHYSICAL_ID})
         response.raise_for_status()
-        return response.json()
+        try:
+            return response.json()
+        except json.JSONDecodeError:
+            print("Error decoding JSON. Response content:", response.content)
+            return None
 
 async def set_status():
     async with httpx.AsyncClient() as client:
         payload = {"physical_id": configs.CONTROLLER_PHYSICAL_ID}
         response = await client.post(f"{configs.SERVER_URL}/api/cservice/set-status/", json=payload)
         response.raise_for_status()
+        
+async def register_controller():
+    async with httpx.AsyncClient() as client:
+        payload = {"physical_id": configs.CONTROLLER_PHYSICAL_ID}
+        response = await client.post(f"{configs.SERVER_URL}/api/cservice/register-controller/", json=payload)
+        response.raise_for_status()
 
 async def periodic_task():
     while True:
         # Get new status data from weather-condition endpoint
         weather_condition = await get_weather_condition()
-        print("Received new status data:", weather_condition)
+        is_registered = weather_condition["is_registered"]
         
-        # TODO to debug: remove it in production
-        await asyncio.sleep(5)
+        if is_registered:
+            print("Received new status data:", json.dumps(weather_condition, indent=2))
+            
+            # TODO to debug: remove it in production
+            await asyncio.sleep(5)
 
-        # Acknowledge the arrival and register of new status data using set-status endpoint
-        await set_status()
-        print("Acknowledged arrival and registered new status data.")
+            # Acknowledge the arrival and register of new status data using set-status endpoint
+            await set_status()
+            print("Acknowledged arrival and registered new status data.")
 
-        # Sleep for 5 minutes before the next iteration
-        await asyncio.sleep(configs.VALIDATION_INTERVAL_IN_SECONDS)
+            # Sleep for 5 minutes before the next iteration
+            await asyncio.sleep(configs.VALIDATION_INTERVAL_IN_SECONDS)
+        else:
+            print("Registration is pending...")
+            await asyncio.sleep(configs.CHECK_REGISTRATION_IN_SECONDS)
+            
 
 @app.on_event("startup")
 async def startup_event():
+    # register_controller()
     # Start the periodic task in the background
     asyncio.create_task(periodic_task())
 
